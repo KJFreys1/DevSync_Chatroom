4423e0c6039797a3bc52e5d6a879063f1abc5ec8
KJFreys1 <kfreyermuth20@gmail.com>
2020-02-19 12:18:08 -0800

    successfully test router for room and post

--- server/db/seed.js
+++ server/db/seed.js
@@ -1,11 +1,32 @@
 const Room = require('../models/Room')
+const Post = require('../models/Post')
 
-Room.deleteMany({}).then(createData)
+Room.deleteMany({}).then(() => {
+    Post.deleteMany({}).then(() => {
+        console.log('db deleted, running seed...')
+        createData()
+    })
+})
 
 function createData() {
     Room.create({
         name: 'Room Test'
+    }).then(room => {
+        Promise.all([
+            Post.create({
+                message: 'post one'
+            }),
+            Post.create({
+                message: 'post two'
+            })
+        ]).then(posts => {
+            posts.forEach(post => {
+                room.posts.push(post)
+                post.room = room
+                post.save()
+            })
+            room.save()
+            console.log('created room with posts')
+        })
     })
-    .then(room => room.save())
-    .then(() => console.log('created room'))
 }
