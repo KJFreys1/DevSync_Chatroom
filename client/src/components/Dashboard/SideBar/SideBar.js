41edd74cb74a6621988ba00108c4c78b91181ae0
KJFreys1 <kfreyermuth20@gmail.com>
2020-08-21 15:46:52 -0700

    added nav menu

--- client/src/components/Dashboard/SideBar/SideBar.js
+++ client/src/components/Dashboard/SideBar/SideBar.js
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Room from './Room/Room'
 
 import './SideBar.css'
 
 function SideBar(props) {
     const button = props.list ? '- Hide Rooms' : '+ List Rooms'
+    const [burgerActive, setBurgerActive] = useState('')
 
     const handleListRooms = e => {
         e.preventDefault()
@@ -12,6 +13,14 @@ function SideBar(props) {
             props.hideListRooms()
         } else {
             props.listRooms()
+        }
+    }
+
+    const handleMenuClick = () => {
+        if (burgerActive.length) {
+            setBurgerActive('')
+        } else {
+            setBurgerActive('burger-active')
         }
     }
 
@@ -34,16 +43,23 @@ function SideBar(props) {
 
     return (
         <section className='sidebar'>
-            <div className='header'>
-                <h1 className='devsync-title'>DevSync</h1>
-                <h2 className='header-greeting'>@{props.name}<span className='logout' onClick={props.handleLogout}>Log out</span></h2>
+            <div className='side-full'>
+                <div className='header'>
+                    <h1 className='devsync-title'>DevSync</h1>
+                    <h2 className='header-greeting'>@{props.name}<span className='logout' onClick={props.handleLogout}>Log out</span></h2>
+                </div>
+                <div className='title-side'>
+                    <h1 className='my-rooms'>My Rooms<div className='add-room-button' onClick={props.showAddRoom}>+</div></h1>              
+                </div>
+                <h6 className='view-all-rooms' onClick={handleListRooms}>{button}</h6>
+                <div className='side-rooms-container'>
+                    {rooms}
+                </div>
             </div>
-            <div className='title-side'>
-                <h1 className='my-rooms'>My Rooms<div className='add-room-button' onClick={props.showAddRoom}>+</div></h1>              
-            </div>
-            <h6 className='view-all-rooms' onClick={handleListRooms}>{button}</h6>
-            <div className='side-rooms-container'>
-                {rooms}
+            <div className={`hamburger ${burgerActive}`} onClick={handleMenuClick}>
+                <div className="ham-line"></div>
+                <div className="ham-line"></div>
+                <div className="ham-line"></div>
             </div>
         </section>
     )
